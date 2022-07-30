489a2955bc04e2b7c89cae6d9e6d789edc3dc14b
Kienyew <aobi100bt@hotmail.com>
2022-07-30 14:22:36 +0800

    Complete section 3.3

--- interpolation.py
+++ interpolation.py
@@ -1,6 +1,8 @@
 import sympy
-import functools
-from sympy.abc import x
+import math
+from sympy.abc import x, c
+from sympy import pi, pprint, prod, product
+import numpy
 
 # sympy.init_printing(use_unicode=True)
 
@@ -52,10 +54,82 @@ def example():
     sympy.pprint(divided_difference(*zip(xs, ys)).expand())
 
 
+def interpolation_error(xs: list[float], f) -> sympy.Expr:
+    n = len(xs)
+    prod = 1
+    for xi in xs:
+        prod *= x - xi
+
+    prod /= sympy.factorial(n)
+    prod *= f.diff(x, n).subs({x: c})
+
+    return prod
+
+
 def additional_examples():
-    print(divided_difference((-2, -9), (-1, -1),
-                             (1, -9), (3, -9), (4, 9)).expand())
+   #  print(divided_difference((-2, -9), (-1, -1), (0, -9), (3, -9), (4, 9)).expand())
+
+    xs = [0, 0.2, 0.4, 0.8, 1.0]
+    ys = [*map(lambda x: 2**x, xs)]
+    print(divided_difference(*zip(xs, ys)).expand())
+    errf = interpolation_error([0, 0.2, 0.4, 0.6, 0.8, 1.0], 2**x)
+    pprint(errf.subs({x: 0.5, c: 1}).evalf())
+    pprint(errf.subs({x: 0.9, c: 1}).evalf())
 
 
-# example()
-additional_examples()
+def e1():
+    xs = [sympy.Float(0), pi/2, pi]
+    ys = [*map(math.sin, xs)]
+
+    # (a)
+    p4 = divided_difference(*zip(xs, ys)).expand()
+    pprint(p4)
+
+    # (b)
+    pprint(p4.subs({x: pi/4}))
+
+    # (c)
+    errf = interpolation_error(xs, sympy.sin(x))
+    pprint(errf.subs({x: pi/4, c: 0}))
+
+
+def e3():
+    xs = numpy.linspace(0, 1, 10)
+    ys = [*map(math.sin, xs)]
+
+    errf = interpolation_error(xs, sympy.exp(-2*x))
+    # (a)
+    pprint(errf)
+    pprint(errf.subs({x: 1/2, c: 0}))
+
+
+def e5():
+    xs = numpy.linspace(0.1, 0.6, 6)
+    err_faction = sympy.prod([(x - xi) for xi in xs])
+    pprint(err_faction.subs({x: 0.35}) / err_faction.subs({x: 0.55}))  # ~ 5/21
+
+
+def c1():
+    xs = [0.6, 0.7, 0.8, 0.9, 1.0]
+    ys = [1.433329, 1.63216, 1.896481, 2.247908, 2.718282]
+
+    p4 = divided_difference(*zip(xs, ys)).expand()
+    pprint(p4)
+    pprint(p4.subs({x: 0.82}))
+    pprint(p4.subs({x: 0.98}))
+
+    errf = interpolation_error(xs, sympy.exp(x**2))
+    pprint(errf.subs({x: 0.82, c: 1}).evalf())
+    pprint(errf.subs({x: 0.98, c: 1}).evalf())
+
+    print(p4 - sympy.exp(x**2))
+
+
+def c3():
+    xs = numpy.linspace(1994, 2003, 10)
+    ys = [67.052e6, 68.008e6, 69.803e6, 72.024e6, 73.4e6,
+          62.024e6, 72.063e6, 74.669e6, 74.487e6, 74.065e6, 76.777e6]
+    print(divided_difference(*zip(xs, ys)))
+
+
+c3()
